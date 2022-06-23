5bf93bfaa74ec7aa8a78c892b892e7870da2af55
ngonga3 <gadlumbwe8@gmail.com>
2022-06-23 10:22:28 +0200

    premier ajout de la methode somme

---
+++ src/Calcul_op.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Calcul_op {
+    public double somme(double x,double y){
+        double s =0.0;
+        s= x+y;
+        return s;
+    }
+    public static void main(String[] args){
+        double a,b,s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("donne la valeur de a :");
+        a = sc.nextDouble();
+        System.out.println("donne la valeur b");
+        b = sc.nextDouble();
+        
+    }
+}
