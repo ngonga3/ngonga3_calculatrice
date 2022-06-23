624441c355b2daf26ce15bcdcc94dcb340e96ce9
ngonga3 <gadlumbwe8@gmail.com>
2022-06-23 10:30:58 +0200

    la methode de multiplication

--- src/Calcul_op.java
+++ src/Calcul_op.java
@@ -5,13 +5,19 @@ public class Calcul_op {
         s= x+y;
         return s;
     }
+    public double multiplication(double x, double y){
+        double res = 0.0;
+        res = x*y;
+        return res;
+    }
+
     public static void main(String[] args){
-        double a,b,s;
+        double a,b,som=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("donne la valeur de a :");
         a = sc.nextDouble();
         System.out.println("donne la valeur b");
         b = sc.nextDouble();
-        
+       som = somme(a,b);
     }
 }
